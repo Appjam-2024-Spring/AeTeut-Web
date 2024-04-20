90114371ed7120a83b97340a56f347d03a3b42ae
MsEmily1020 <peace10200khs@gmail.com>
2024-04-20 21:43:45 +0900

    Feat :: kakao map marker 표시

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,13 +9,19 @@ declare global {
 
 export default function Main() {
   useEffect(() => {
-    let container = document.getElementById(`map`); // 지도를 담을 영역의 DOM 레퍼런스
-    let options = {
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
+    const container = document.getElementById("map");
+    const mainPosition = new window.kakao.maps.LatLng(33.453502, 126.569894);
+    const options = {
+      center: mainPosition, // 지도의 중심 좌표
       level: 3, // 지도의 레벨(확대, 축소 정도)
     };
 
-    let map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+    const mainMarker = new window.kakao.maps.Marker({
+      position: mainPosition,
+    });
+
+    const map = new window.kakao.maps.Map(container, options);
+    mainMarker.setMap(map); // 메인 위치 set
   }, []);
 
   return (
